c312894a1b499ec08c5510ee7c8e88d53371442b
João Fabris <jvfabris21@gmail.com>
2025-05-26 11:28:35 -0300

    feat: add ex1 to ex4

---
+++ ex3.java
@@ -0,0 +1,12 @@
+// Imagine que você é responsável por 
+//registrar quantos clientes entram na loja a cada hora. 
+//Crie um programa que imprima os números de 1 a 10, simulando a contagem de clientes ao longo de uma hora.
+
+
+public class ex3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) { // loop from 1 to 10
+            System.out.println("client number: " + i); // print the current customer number
+        }
+    }
+}
